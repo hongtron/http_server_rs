50038345f6f082e5eec9cda2600e58e36d37e654
ali <github@alihong.net>
2022-11-08 16:50:57 -0600

    finish multithreading

--- src/main.rs
+++ src/main.rs
@@ -12,14 +12,14 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     let pool = ThreadPool::new(4);
 
-    for stream in listener.incoming() {
+    for stream in listener.incoming().take(2) {
         let stream = stream.unwrap();
-        println!("Connection established");
-
         pool.execute(|| {
             handle_connection(stream);
         });
     }
+
+    println!("Shutting down.");
 }
 
 fn handle_connection(mut stream: TcpStream) {
@@ -66,4 +66,5 @@ fn handle_connection(mut stream: TcpStream) {
     };
 
     stream.write_all(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
